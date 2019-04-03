dccb3221d3ca7ed7cc642478c8257fd81f9d03c3
lootster <sheldonkhlim@gmail.com>
2019-04-03 22:15:05 +0800

    Initial commit

---
+++ build/precache-manifest.c340aa15013ea70115b44c71b5fb174f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b5e2613c8378c94488c",
+    "url": "/static/css/main.5feebe7c.chunk.css"
+  },
+  {
+    "revision": "9b5e2613c8378c94488c",
+    "url": "/static/js/main.9b5e2613.chunk.js"
+  },
+  {
+    "revision": "85042e97a37d71d7ff89",
+    "url": "/static/js/1.85042e97.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0ebc18432c0a71cfd876a27933f91a4a",
+    "url": "/static/media/mongoleaf.0ebc1843.png"
+  },
+  {
+    "revision": "6c93bd2010905059e08c8b522adfc219",
+    "url": "/static/media/pixelatedLeaf.6c93bd20.svg"
+  },
+  {
+    "revision": "ff4033b01e35fba05ebebcd9cbdcd6cd",
+    "url": "/index.html"
+  }
+];
